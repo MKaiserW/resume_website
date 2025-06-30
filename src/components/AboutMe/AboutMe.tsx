4898e659b3b90837313d8c257cd6db8110ccdfe2
MKaiserW <maxkaiserwilhelm@gmail.com>
2025-06-30 03:51:17 +0000

    temp

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -76,6 +76,37 @@ export default function AboutMe() {
                     <h4 className='pratt-name-sub'>An RTX Business</h4>
                 </div>
             </div>
+            <div className="about-container">
+                <div className="about-profile-card">
+                    <img
+                        src={UconnLogo}
+                        alt="University of Connecticut Logo"
+                        className="about-logo-university"
+                    />
+                    <h2>University of Connecticut</h2>
+                </div>
+                <div>
+                    <h2 className="about-education-section">Education</h2>
+                    <div className="about-education-grid">
+                        <div className="about-education-item">
+                            <h3>Degree</h3>
+                            <p>Bachelor of Science in Computer Science</p>
+                        </div>
+                        <div className="about-education-item">
+                            <h3>Concentrations</h3>
+                            <p>Software Engineering<br />Systems and Networks</p>
+                        </div>
+                        <div className="about-education-item">
+                            <h3>Minor</h3>
+                            <p>Mathematics</p>
+                        </div>
+                        <div className="about-education-item">
+                            <h3>GPA</h3>
+                            <p>3.8</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
